16049926aea6e611d374feb2edf63342ace1c3e8
Joshua Warner <joshnwarner56@gmail.com>
2016-07-30 13:40:17 -0500

    Pixel generaton file added, generated 512x512 image

--- src/me/joshuawarner/Main.java
+++ src/me/joshuawarner/Main.java
@@ -1,8 +1,39 @@
 package me.joshuawarner;
 
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //image dimension
+        int width = 512;
+        int height = 512;
+        //create buffered image object img
+        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        //file object
+        File f = null;
+
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                int a = (int)(Math.random()*256); //alpha
+                int r = (int)(Math.random()*256); //red
+                int g = (int)(Math.random()*256); //green
+                int b = (int)(Math.random()*256); //blue
+
+                int p = (a<<24) | (r<<16) | (g<<8) | b;
+
+                img.setRGB(x, y, p);
+            }
+        }
+
+        try{
+            f = new File("/Users/joshua/desktop/Output.png");
+            ImageIO.write(img, "png", f);
+        }catch(IOException e){
+            System.out.println("Error: " + e);
+        }
     }
 }
